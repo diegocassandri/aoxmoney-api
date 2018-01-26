d7129032ae48bc8e19280c19ccd02cbce208d32f
diegocassandri <diego_aox886@hotmail.com>
2018-01-26 00:12:40 -0200

    Inicial

---
+++ src/main/java/com/aox/aoxmoney/api/AoxmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.aox.aoxmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoxmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoxmoneyApiApplication.class, args);
+
+	}
+}
