3adb0c6b62f307d5fe3ca82c09e024bcef713ec4
diegocassandri <diegocassandri@10.1.1.8>
2018-02-06 22:40:44 -0200

    Implementado o cadastro de pessoa

---
+++ src/main/java/com/aox/aoxmoney/api/repository/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.aox.aoxmoney.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aox.aoxmoney.api.model.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+
+}
---
+++ src/main/java/com/aox/aoxmoney/api/resource/PessoaResource.java
@@ -0,0 +1,50 @@
+package com.aox.aoxmoney.api.resource;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.aox.aoxmoney.api.repository.PessoaRepository;
+import com.aox.aoxmoney.api.model.Pessoa;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+   
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@GetMapping
+	private List<Pessoa> listar(){
+		return pessoaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa,HttpServletResponse response) {
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(pessoaSalva.getCodigo()).toUri();
+		response.setHeader("Location",uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(pessoaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+		Pessoa pessoa = pessoaRepository.findOne(codigo);
+		 return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+	}
+}
